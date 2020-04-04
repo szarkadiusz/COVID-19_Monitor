512125956649d31c0f84899f21958d30fbb039eb
Arek Szymanski <arkadiuszsz@gmail.com>
2020-04-04 23:14:04 +0200

    Added JSON handling code cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import javax.swing.*;
 import java.awt.*;
-import java.util.Collections;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main extends JFrame {
     private static JFrame jFrame;
@@ -16,13 +23,14 @@ public class Main extends JFrame {
     private static JLabel updateDate;
     private static JLabel country;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
         jFrame = new JFrame("COVID-19 MONITOR");
 
         jPanel = new JPanel(new GridLayout(5, 2));
         jPanel.setBackground(Color.darkGray);
-        jPanel.setSize(800,800);
+        jPanel.setSize(800, 800);
 
         countryCreate();
         updateDateCreate();
@@ -35,7 +43,10 @@ public class Main extends JFrame {
         deathsNewCreate();
         curedNewCreate();
 
-        jPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        createHttpResponse();
+
+        jPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 
         jFrame.setVisible(true);
@@ -45,8 +56,11 @@ public class Main extends JFrame {
         jFrame.pack();
     }
 
-    private static void countryCreate() {
-        country=new JLabel("Country: ");
+
+    private static void countryCreate() throws IOException {
+
+        String getCountry = createHttpResponse().getString("country");
+        country = new JLabel("Country: " + getCountry);
         jPanel.add(country);
         country.setForeground(Color.white);
         setHeightOfFont_LOW(country);
@@ -54,18 +68,19 @@ public class Main extends JFrame {
 
     }
 
-    private static void updateDateCreate() {
-
-        updateDate=new JLabel("Updated: ");
+    private static void updateDateCreate() throws IOException {
+        String getUpdateDate = createHttpResponse().getString("time");
+        updateDate = new JLabel("Updated: " + getUpdateDate);
         jPanel.add(updateDate);
         updateDate.setForeground(Color.white);
         setHeightOfFont_LOW(updateDate);
 
     }
 
-    private static void infectedTotalCreate() {
-
-        infectedTotal=new JLabel("Infected: ");
+    private static void infectedTotalCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("cases");
+        String getTotal = String.valueOf(getCases.getInt("total"));
+        infectedTotal = new JLabel("Infected: " + getTotal);
         jPanel.add(infectedTotal);
         infectedTotal.setForeground(Color.yellow);
         setHeightOfFont_HIGH(infectedTotal);
@@ -73,18 +88,21 @@ public class Main extends JFrame {
 
     }
 
-    private static void activeCreate() {
-
-        active=new JLabel("Active: ");
+    private static void activeCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("cases");
+        String getActive = String.valueOf(getCases.getInt("active"));
+        active = new JLabel("Active: " + getActive);
         jPanel.add(active);
         active.setForeground(Color.orange);
         setHeightOfFont_HIGH(active);
 
     }
 
-    private static void infectedTotalNewCreate() {
+    private static void infectedTotalNewCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("cases");
+        String getNew = String.valueOf(getCases.getString("new"));
 
-        infectedTotalNew=new JLabel("new infected +");
+        infectedTotalNew = new JLabel("Todays new infected " + getNew);
         jPanel.add(infectedTotalNew);
         infectedTotalNew.setForeground(Color.yellow);
         setHeightOfFont_LOW(infectedTotalNew);
@@ -92,9 +110,11 @@ public class Main extends JFrame {
 
     }
 
-    private static void activeNewCreate() {
+    private static void activeNewCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("cases");
+        String getNew = String.valueOf(getCases.getString("new"));
 
-        activeNew=new JLabel("active new +");
+        activeNew = new JLabel("Todays new active " + getNew);
         jPanel.add(activeNew);
         activeNew.setForeground(Color.orange);
         setHeightOfFont_LOW(activeNew);
@@ -102,9 +122,11 @@ public class Main extends JFrame {
 
     }
 
-    private static void deathsCreate() {
+    private static void deathsCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("deaths");
+        String getDeaths = String.valueOf(getCases.getInt("total"));
 
-        deaths=new JLabel("Deaths: ");
+        deaths = new JLabel("Deaths: " + getDeaths);
         jPanel.add(deaths);
         deaths.setForeground(Color.red);
         setHeightOfFont_HIGH(deaths);
@@ -112,18 +134,21 @@ public class Main extends JFrame {
 
     }
 
-    private static void curedCreate() {
-
-        cured=new JLabel("Cured: ");
+    private static void curedCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("cases");
+        String getRecovered = String.valueOf(getCases.getInt("recovered"));
+        cured = new JLabel("Cured: " + getRecovered);
         jPanel.add(cured);
         cured.setForeground(Color.green);
         setHeightOfFont_HIGH(cured);
 
     }
 
-    private static void deathsNewCreate() {
+    private static void deathsNewCreate() throws IOException {
+        JSONObject getCases = createHttpResponse().getJSONObject("deaths");
+        String getDeathsNew = String.valueOf(getCases.getString("new"));
 
-        deathsNew= new JLabel("deaths new +");
+        deathsNew = new JLabel("Todays new deaths " + getDeathsNew);
         jPanel.add(deathsNew);
         deathsNew.setForeground(Color.red);
         setHeightOfFont_LOW(deathsNew);
@@ -133,7 +158,7 @@ public class Main extends JFrame {
 
     private static void curedNewCreate() {
 
-        curedNew = new JLabel("cured new +");
+        curedNew = new JLabel("No info");
         jPanel.add(curedNew);
         curedNew.setForeground(Color.green);
         setHeightOfFont_LOW(curedNew);
@@ -149,5 +174,37 @@ public class Main extends JFrame {
         jLabel.setFont(new Font("Helvetica", Font.BOLD, 20));
     }
 
+    public static OkHttpClient createHttpClient() {
 
+        return new OkHttpClient();
+    }
+
+    public static Request createHttpRequest() {
+        return new Request.Builder()
+                .url("https://covid-193.p.rapidapi.com/statistics?country=Poland")
+                .get()
+                .addHeader("x-rapidapi-host", "covid-193.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "763f47715fmsh5e3be17d0369764p17cd41jsn0c89dde4e190")
+                .build();
+
+
+    }
+
+    private static JSONObject createHttpResponse() throws IOException {
+
+        OkHttpClient client = createHttpClient();
+        Request request = createHttpRequest();
+        Response response = client.newCall(request).execute();
+
+        JSONObject getParams = new JSONObject(response.body().string());
+
+        JSONArray responseArray = getParams.getJSONArray("response");
+
+        JSONObject getParamsofObject0 = responseArray.getJSONObject(0);
+
+        return getParamsofObject0;
+
+    }
 }
+
+
